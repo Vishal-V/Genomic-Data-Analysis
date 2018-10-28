8a8e3a726d9af3ade43f7d87ee8a2e87ef9bcad5
Vishal-V <vishal114186@gmail.com>
2018-10-28 20:52:15 +0530

    Improved monte_carlo.R

--- PH525.1x Exercise Solutions/monte_carlo.R
+++ PH525.1x Exercise Solutions/monte_carlo.R
@@ -1,15 +1,25 @@
-data <- read.csv("mice_pheno.csv")
-data <- na.omit(data)
 library(dplyr)
 library(rafalib)
-controlpop <- filter(data,Diet=="chow") %>% select(Bodyweight) %>% unlist
-mean <- mean(controlpop)
-sd <- sd(controlpop)
+library(tidyverse)
+
+irradiance <- read.csv("solar_radiation.csv")
+cloud <- read.csv("cloud_cover.csv")
+wind <- read.csv("wind_speed.csv")
+irradiance <- na.omit(data)
+
+data <- data.frame(irradiance, cloud, wind)
+
+control <- filter(data, cloud > 1073 & wind < 7.87) %>% select(irradiance) %>% unlist
+mean <- mean(control)
+sd <- sd(control)
+
 set.seed(1)
+
 generate <- function(N,mean,sd){
   sample1 <- rnorm(N,mean,sd)
   sqrt(5)*mean(sample1)/sd(sample1)
   
 }
-generate(5,mean,sd)
+
+samples <- generate(250, mean, sd)
 
